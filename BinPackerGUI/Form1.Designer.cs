a587b29fbc820208cb62c115738fa7c2b9306c54
kociokwik <sebastianjozwiakwwa@gmail.com>
2022-02-06 00:20:02 +0100

    Missing winforms code

--- BinPackerGUI/Form1.Designer.cs
+++ BinPackerGUI/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace BinPackerGUI
+﻿namespace BinPackerGUI
 {
     partial class Form1
     {
@@ -29,13 +28,65 @@ namespace BinPackerGUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Drawing_Picture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Drawing_Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Drawing_Picture
+            // 
+            this.Drawing_Picture.Location = new System.Drawing.Point(12, 12);
+            this.Drawing_Picture.Name = "Drawing_Picture";
+            this.Drawing_Picture.Size = new System.Drawing.Size(889, 637);
+            this.Drawing_Picture.TabIndex = 1;
+            this.Drawing_Picture.TabStop = false;
+            this.Drawing_Picture.Paint += new System.Windows.Forms.PaintEventHandler(this.Drawing_Picture_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(907, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Packed count: ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(907, 61);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(372, 547);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(5000, 2000);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1321, 736);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Drawing_Picture);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Drawing_Picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox Drawing_Picture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- BinPackerGUI/Form1.cs
+++ BinPackerGUI/Form1.cs
@@ -1,20 +1,156 @@
-﻿using System;
+﻿using BinPacker2D;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace BinPackerGUI
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+	public partial class Form1 : Form
+	{
+		Graphics drawing_table;
+		
+		double Table_Width;
+		double Table_Height;
+
+        private List<RowElement> _inputElements;
+        private Packer _packer2D = new Packer();
+
+		public Form1(double tw, double th)
         {
             InitializeComponent();
+
+			this.Table_Height = th;
+            this.Table_Width = tw;
+            CreateSampleElements();
+
+			var finishedElements = _packer2D.PackElements(400, 520, 5, 5, 5, 5, 5, _inputElements);
+
+            this.DoubleBuffered = true;
+            this.AutoScroll = true;
+            
+            Initialize_Graphics();
+            DrawSummaryGraphics(finishedElements);
+            label1.Text = $"Packed count: {finishedElements.Count}";
+
+            var tLines = new List<string>();
+            foreach (var e in finishedElements)
+            {
+                var st = $"{$"{e.sortIndex}",-3} {$"X: {e.posX}",-12} {$"Y: {e.posY}",-12} {$"W: {e.width}",-12} {$"H: {e.height}",-12}";
+                tLines.Add(st);
+            }
+
+            richTextBox1.Lines = tLines.ToArray();
+		}
+
+		private void CreateSampleElements()
+        {
+            _inputElements = new List<RowElement>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                _inputElements.Add(new RowElement
+                {
+                    width = rand.Next(80, 100),
+                    height = rand.Next(40, 50)
+                });
+            }
+
+			for (int i = 0; i < 15; i++)
+            {
+				_inputElements.Add(new RowElement
+                {
+                    width = rand.Next(60, 70), 
+                    height = rand.Next(36, 50)
+				});
+			}
+
+            for (int i = 0; i < 60; i++)
+            {
+                _inputElements.Add(new RowElement
+                {
+                    width = rand.Next(10, 15),
+                    height = rand.Next(10, 15)
+                });
+            }
+
+
+		}
+
+		/// <summary>
+		/// Initialize new drawing table
+		/// </summary>
+		private void Initialize_Graphics()
+		{
+			Drawing_Picture.Image = new Bitmap(Drawing_Picture.Width, Drawing_Picture.Height);
+			
+			drawing_table = Graphics.FromImage(Drawing_Picture.Image);
+			drawing_table.SmoothingMode = SmoothingMode.HighQuality;
+            drawing_table.TranslateTransform(0, Drawing_Picture.Height - 1);
+			
+			drawing_table.Clear(Color.White);
+		}
+
+		private void Drawing_Picture_Paint(object sender, PaintEventArgs e)
+		{
+			base.OnPaint(e);
         }
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+
+			e.Graphics.ScaleTransform(1.0f, -1.0f);
+			e.Graphics.TranslateTransform(0, -this.ClientRectangle.Height);
+			e.Graphics.DrawLine(Pens.Black, 0, 0, 100, 100);
+		}
+
+		private void DrawSummaryGraphics(List<RowElement> packedElements)
+        {
+            int multiply = 1;
+            int posXoffset = 0;
+            int posYoffset = 400;
+
+            Bitmap bitmap = new Bitmap(Drawing_Picture.Width, Drawing_Picture.Height,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            Pen pencil = new Pen(Color.Black, 1);
+            Pen pencil_2 = new Pen(Color.Green, 1);
+            drawing_table = Graphics.FromImage(bitmap);
+
+            using (System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red))
+            {
+                double totalHeight = 0;
+				foreach (BinPacker2D.RowElement element in packedElements)
+                {
+                    totalHeight += element.height;
+                    int posX = multiply * int.Parse(element.posX.ToString()) + posXoffset;
+                    int w = multiply * int.Parse(element.width.ToString());
+                    int h = multiply * int.Parse(element.height.ToString());
+                    int posY = 0;
+                    posY = multiply * posYoffset - int.Parse(element.height.ToString()) - int.Parse(element.posY.ToString());
+
+                    drawing_table.FillRectangle(myBrush, new Rectangle(posX, posY, w, h));
+                    drawing_table.DrawRectangle(pencil, new Rectangle(posX, posY, w, h));
+                }
+				int ax = multiply * posXoffset;
+                int ay = multiply * 0;
+                int aw = multiply * int.Parse(Table_Width.ToString());
+                int ah = multiply * int.Parse(this.Table_Height.ToString());
+
+                drawing_table.DrawRectangle(pencil_2, new Rectangle(ax, ay, aw, ah));
+
+
+            } 
+            Drawing_Picture.Image = bitmap;
+        }
+
+        
+
+       
     }
+
+
 }
