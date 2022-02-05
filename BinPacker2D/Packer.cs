d1b2dea33f52d763804e6f5f4f3ac1e3e637920d
kociokwik <sebastianjozwiakwwa@gmail.com>
2022-02-06 00:49:29 +0100

    adjust description

--- BinPacker2D/Packer.cs
+++ BinPacker2D/Packer.cs
@@ -20,8 +20,8 @@ namespace BinPacker2D
          * - Each time we create a rotated copy of an element we also create new Variant.
          * 2. We do the same for the next element.
          * - If it's height is smaller than previous element, then we create a SPLIT of this row => one row has height of the currently inserted element and we have second row on top of it for later use.
-         * 3. We insert elements along MAIN ROW Y coordinate (while also saving row SPLITS for later use + creating copied Variants with rotated element) until we run out of elements, or next element can't fit in this row.
-         * 4. When the MAIN ROW is done (no more free space in it's Y axis) and there are still unpacked elements, we go back to previous empty SPLIT row where next element can fit and treat it as our new ROW.
+         * 3. We insert elements along MAIN ROW X coordinate (while also saving row SPLITS for later use + creating copied Variants with rotated element) until we run out of elements, or next element can't fit in this row.
+         * 4. When the MAIN ROW is done (no more free space in it's X axis) and there are still unpacked elements, we go back to previous empty SPLIT row where next element can fit and treat it as our new ROW.
          * 5. When finally there is no more free space in the MAIN ROW (we checked all the inside rows/splits), we create a new MAIN ROW on top of the first one. Repeat from step 1.
          * 6. When we don't have free elements or free space in the whole TABLE we can compare all of the outcomes (VariantGroups). Each of them holds it's own rows (RowVariant) and packed elements.
          * 7. Select VairantGroup with most packed elements. If 2 VariantsGroups have same number of elements, the we select one with lower Y coordinate (less space used).
